030f37ffa69173717b27d13e4ec5328f6ec868df
Ray Fix <rayfix@gmail.com>
2021-08-28 17:04:43 -0700

    Initial Commit

---
+++ M25a/M25aApp.swift
@@ -0,0 +1,17 @@
+//
+//  M25aApp.swift
+//  M25a
+//
+//  Created by Ray Fix on 8/28/21.
+//
+
+import SwiftUI
+
+@main
+struct M25aApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
